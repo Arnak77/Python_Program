347832935298a5b345128a2a4b71f143a666d9d3
Arnak Jain <arnakjain87421@gmail.com>
2024-04-24 23:44:01 +0530

    Add files via upload

---
+++ PYTHON PROGRAM/Swap(Third_var).py
@@ -0,0 +1,16 @@
+#Swa(Third_var).py
+a=int(input("enter no of a="))
+b=int(input("enter no of b="))
+
+print("="*50)
+print("original value of a={}".format(a))
+print("original value of b={}".format(b))
+print("="*50)
+#swapping logic with (Third_var)
+c=a
+a=b
+b=c
+print("swapped value of a={}".format(a))7
+print("swapped value of b={}".format(b))
+
+print("="*50)
