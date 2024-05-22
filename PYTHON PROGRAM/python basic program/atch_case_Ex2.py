bbaab44548367839061cc9f257b83f3b294ffdeb
Arnak Jain <arnakjain87421@gmail.com>
2024-05-22 11:28:38 +0530

    Add files via upload

---
+++ PYTHON PROGRAM/python basic program/atch_case_Ex2.py
@@ -0,0 +1,28 @@
+
+
+
+
+print("="*50)
+print("Figure Area Calculate:")
+print("="*50)
+print("R.Rectangle")
+print("S.Square")
+print("C.Circle")
+print("T.Triangle")
+print("")
+print("="*50)
+ch=input("Enter Your Choice:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
