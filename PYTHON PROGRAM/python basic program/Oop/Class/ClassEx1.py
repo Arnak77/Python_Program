3516f93148cd5c4a4e7d46d320820c2b7ad1d579
Arnak Jain <arnakjain87421@gmail.com>
2024-06-11 01:49:39 +0530

    Add files via upload

---
+++ PYTHON PROGRAM/python basic program/Oop/Class/ClassEx1.py
@@ -0,0 +1,10 @@
+class Student():pass
+
+
+s1=Student()
+s2=Student()
+
+print("-------------------------------------------")
+print('content of s1={}'.format(s1.__dict__))
+print('content of s2={}'.format(s2.__dict__))
+print("-------------------------------------------")
