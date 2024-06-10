7d894ffcd84555ded30b95962f91ccc42696f600
Arnak Jain <arnakjain87421@gmail.com>
2024-06-10 12:06:31 +0530

    Delete PYTHON PROGRAM/Parameter of Circle.py

--- PYTHON PROGRAM/Parameter of Circle.py
+++
@@ -1,8 +0,0 @@
-#program for cal Parameter of circle
-#Parameter of circle
-r=float(input("Parameter of circle="))
-pc=2*3.14*r
-print("="*50)
-print("Radius{}".format(r))
-print("Parameter of circle{}".format(pc))
-print("="*50)
